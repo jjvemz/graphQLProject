7c22448ad784f28c271edc901cffc2e3bd767a50
juanjo2331 <40677444+jjvemz@users.noreply.github.com>
2023-08-29 11:23:00 -0400

    CREATE and DELETE methods are complete

--- core/schema.py
+++ core/schema.py
@@ -6,6 +6,29 @@ class BookType(DjangoObjectType):
     class Meta:
         model = Book
         fields= ("id","title","desc", "created_at", "updated_at")
+
+class CreateBookMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        desc = graphene.String()
+
+    book = graphene.Field(BookType)
+
+    def mutate(self, info, title, desc):
+        book = Book(title=title, desc=desc)
+        book.save()
+        return CreateBookMutation(book=book)
+    
+class DeleteBookMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+    
+    message = graphene.String()
+
+    def mutate(self, info, id):
+        book= Book.objects.get(pk=id)
+        book.delete()
+        return DeleteBookMutation(message="Book Deleted")
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello World!")
     books = graphene.List(BookType)
@@ -16,5 +39,9 @@ class Query(graphene.ObjectType):
 
     def resolve_book(self, info, id):
         return Book.objects.get(pk=id)
+    
+class Mutation(graphene.ObjectType):
+    create_book = CreateBookMutation.Field()
+    delete_book = DeleteBookMutation.Field() 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation = Mutation)
