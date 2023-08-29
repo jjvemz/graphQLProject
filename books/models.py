351d7f46c81fe6e855bb4972f5b3a4642a334a88
juanjo2331 <40677444+jjvemz@users.noreply.github.com>
2023-08-29 10:35:03 -0400

    GetAll() and GetOne() are done!

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField('Title', max_length=250)
     desc = models.TextField()
     created_at= models.DateTimeField(auto_now_add=True)
--- core/schema.py
+++ core/schema.py
@@ -1,6 +1,20 @@
 import graphene
+from graphene_django import DjangoObjectType
+from books.models import Book
 
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields= ("id","title","desc", "created_at", "updated_at")
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello")
+    hello = graphene.String(default_value="Hello World!")
+    books = graphene.List(BookType)
+    book = graphene.Field(BookType, id=graphene.ID())
+
+    def resolve_books(self, info):
+        return Book.objects.all()
+
+    def resolve_book(self, info, id):
+        return Book.objects.get(pk=id)
 
 schema = graphene.Schema(query=Query)
